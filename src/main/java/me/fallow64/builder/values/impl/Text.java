cb18506411ec6c197cfc7ef317246736be1e0ad0
fallow64 <fallow640@gmail.com>
2022-08-19 16:27:44 -0500

    updated .gitignore and added support for color codes

--- src/main/java/me/fallow64/builder/values/impl/Text.java
+++ src/main/java/me/fallow64/builder/values/impl/Text.java
@@ -7,15 +7,23 @@ public class Text implements CodeValue {
     private final String value;
 
     public Text(String value) {
-        this.value = value;
+        // converts &a to &a
+        // yoinked directly from spigot
+        char[] b = value.toCharArray();
+        for (int i = 0; i < b.length - 1; i++) {
+            if (b[i] == '&' && "0123456789AaBbCcDdEeFfKkLlMmNnOoRrXx".indexOf(b[i + 1]) > -1) {
+                b[i] = '§';
+                b[i + 1] = Character.toLowerCase(b[i + 1]);
+            }
+        }
+        this.value = new String(b);
     }
 
     public String getValue() {
         return value;
     }
 
-    // "name" is in string so that it supports %math
-    // {"id": "num","data": {"name": "1"}}
+    // {"id": "txt","data": {"name": "yo"}}
     @Override
     public String serialize() {
         return "{\"id\": \"txt\",\"data\": {\"name\": \"" + value + "\"}}";
